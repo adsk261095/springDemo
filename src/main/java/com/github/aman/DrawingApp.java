b619aca8245c17ca7abc992e26931b8fbe7ab9b0
adsk261095 <adsk261095@gmail.com>
2020-01-07 10:36:29 +0530

    creating object using ApplicationContext

--- src/main/java/com/github/aman/DrawingApp.java
+++ src/main/java/com/github/aman/DrawingApp.java
@@ -1,21 +1,28 @@
 package com.github.aman;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {
 //        Triangle triangle = new Triangle();
 
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+//        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//        /**
+//         * spring bean factory will give
+//         * me triangle object
+//         * this object is present in spring container
+//         * its lifecycle is managed by spring IOC
+//         */
+//        Triangle triangle = (Triangle) factory.getBean("triangle");
+
         /**
-         * spring bean factory will give
-         * me triangle object
-         * this object is present in spring container
-         * its lifecycle is managed by spring IOC
+         * we need to move spring.xml to the class path
+         * in resorces in case of maven project
          */
-        Triangle triangle = (Triangle) factory.getBean("triangle");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
 
         triangle.draw();
     }
