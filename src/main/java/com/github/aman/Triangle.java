f77ea279872e88628493aed0a9ff82de3f18ef47
adsk261095 <adsk261095@gmail.com>
2020-01-12 16:12:05 +0530

    using ApplicationContextAware and BeanNameAware

--- src/main/java/com/github/aman/Triangle.java
+++ src/main/java/com/github/aman/Triangle.java
@@ -1,9 +1,15 @@
 package com.github.aman;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import java.util.List;
 
-public class Triangle {
+public class Triangle implements ApplicationContextAware, BeanNameAware {
     private List<Point> points;
+    private ApplicationContext context;
 
     public void draw(){
         System.out.println("Drawing Triangle");
@@ -18,5 +24,13 @@ public class Triangle {
 
     public void setPoints(List<Point> points) {
         this.points = points;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
+    public void setBeanName(String beanName) {
+        System.out.println("Bean name is : " + beanName);
     }
 }
