ba3f7fc0d7ab520827cfa3e79cac2cbbe1a86bd2
adsk261095 <adsk261095@gmail.com>
2020-01-14 10:30:47 +0530

    using @Required annotation to prevent NullPointerException at runtime

--- src/main/java/com/github/aman/circle.java
+++ src/main/java/com/github/aman/circle.java
@@ -1,13 +1,17 @@
 package com.github.aman;
 
+import org.springframework.beans.factory.annotation.Required;
+
 public class circle implements shape {
+
     private Point center;
 
     public Point getCenter() {
         return center;
     }
-
+    @Required
     public void setCenter(Point center) {
+        //will throw error at the time when beans are being initialized
         this.center = center;
     }
 
