6b42d17e898dce642c7563d7ffe9bc11cd91be8d
adsk261095 <adsk261095@gmail.com>
2020-01-15 13:52:54 +0530

    using @component and component scan to tell spring to scan all classes for beans

--- src/main/java/com/github/aman/circle.java
+++ src/main/java/com/github/aman/circle.java
@@ -3,7 +3,9 @@ package com.github.aman;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
 
+@Component
 public class circle implements shape {
 
     /**
