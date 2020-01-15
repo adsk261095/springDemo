48a0924d41a930473727cdf1f6dbac38a8bba8d4
adsk261095 <adsk261095@gmail.com>
2020-01-15 12:42:51 +0530

    using qualifier annotation

--- src/main/java/com/github/aman/circle.java
+++ src/main/java/com/github/aman/circle.java
@@ -1,22 +1,25 @@
 package com.github.aman;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 
 public class circle implements shape {
 
+    /**
+     * Autowired annotation works like:
+     * first look for autowiring by type
+     * if multiple beans of same type then do autowiring by name
+     */
+    @Autowired
+    @Qualifier("point1")
     private Point center;
 
     public Point getCenter() {
         return center;
     }
 
-    @Autowired
-    /**
-     * Autowired annotation works like:
-     * first look for autowiring by type
-     * if multiple beans of same type then do autowiring by name
-     */
+
     public void setCenter(Point center) {
         //will throw error at the time when beans are being initialized
         this.center = center;
